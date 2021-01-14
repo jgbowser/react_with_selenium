6c98abf8e2d268dddf221b8b2607cd1284b524a9
John <john.bow7720@gmail.com>
2021-01-14 10:36:29 -0700

    selenium successfully locates h1 and gets text

--- test/example.test.js
+++ test/example.test.js
@@ -1,5 +1,5 @@
 const { expect } = require('chai');
-const { Builder, By, Key, until, } = require('selenium-webdriver');
+const { Builder, By } = require('selenium-webdriver');
 
 describe('making sure tests work', () => {
   it('correctly adds 1 + 1', () => {
@@ -19,4 +19,17 @@ describe('making sure selenium is working', () => {
   });
 
   after('kill browser', async () => driver.quit());
-})
+});
+
+describe('another selenium practice test', () => {
+  const driver = new Builder().forBrowser('firefox').build();
+
+  it('navigates to page and gets header text', async () => {
+    await driver.get('http://localhost:3000/');
+    const headerText = await driver.findElement(By.id('header')).getText();
+
+    expect(headerText).to.eql('Hello Selenium');
+  });
+
+  after('kill browser', async () => driver.quit());
+});
