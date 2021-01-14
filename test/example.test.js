2ad82e5f6eed517d33b4c4027dfcfc36cf3781b8
John <john.bow7720@gmail.com>
2021-01-14 10:26:32 -0700

    successful example selenium test

--- test/example.test.js
+++ test/example.test.js
@@ -1,8 +1,22 @@
 const { expect } = require('chai');
+const { Builder, By, Key, until, } = require('selenium-webdriver');
 
 describe('making sure tests work', () => {
   it('correctly adds 1 + 1', () => {
     const sum = 1 + 1;
     expect(sum).to.eql(2);
-  })
+  });
+});
+
+describe('making sure selenium is working', () => {
+  const driver = new Builder().forBrowser('firefox').build();
+
+  it('navigates to app and gets title', async () => {
+    await driver.get('http://localhost:3000/');
+    const title = await driver.getTitle();
+
+    expect(title).to.eql('Onboarding');
+  });
+
+  after('kill browser', async () => driver.quit());
 })
