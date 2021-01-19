35d8ac3d73619aa8f0bedd6c9f4181f9e4444850
John <john.bow7720@gmail.com>
2021-01-19 11:44:12 -0700

    set up a quick input validation when focus changes

--- src/OwnerForm.js
+++ src/OwnerForm.js
@@ -6,6 +6,19 @@ function OwnerForm() {
   const [ address, setAddress ] = useState('');
   const [ ssn, setSsn ] = useState('');
   const [ success, setSuccess ] = useState(false);
+  const [ errors, setErrors ] = useState({
+    name: '',
+    address: '',
+    ssn: ''
+  })
+
+  function validateSsn() {
+    if(ssn === '') {
+      return setErrors({...errors, ssn: 'SSN is required'})
+    } else if(ssn.length < 9 || ssn.length > 9) {
+      return setErrors({...errors, ssn: 'SSN must be 9 characters'})
+    }
+  }
 
   function handleSubmit(e) {
     e.preventDefault()
@@ -37,7 +50,9 @@ function OwnerForm() {
           name='ssn'
           value={ssn}
           onChange={(e => setSsn(e.target.value) )}
+          onBlur={() => validateSsn()}
         />
+        {errors.ssn && <p>{errors.ssn}</p>}
         <button id='submit' type='submit' onClick={(e) => handleSubmit(e)}>Submit</button>
       </form>
       {success && <h2>Form Submitted</h2>}
@@ -45,4 +60,4 @@ function OwnerForm() {
   );
 }
 
-export default OwnerForm
+export default OwnerForm;
